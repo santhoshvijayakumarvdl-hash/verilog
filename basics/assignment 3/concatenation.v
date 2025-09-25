8a36f8924b7f6f3977448f4f1ad6a18925fbfdb0
santhoshvijayakumarvdl-hash <santhoshvijayakumarvdl@gmail.com>
2025-09-25 12:40:20 +0530

    Create concatenation.v

---
+++ basics/assignment 3/concatenation.v
@@ -0,0 +1,16 @@
+module desi(
+  input [2:0] a,b,c,
+  output [2:0] y
+  //output carry
+       );
+     assign y={a,b[0],c[1]};
+    
+     assign a=1'b1;
+     assign b=3'b111;
+     assign c=3'b101;
+
+   
+    initial begin
+    $monitor(" a=%b b=%b   %b %b  ",$realtime,a,b,c,y);
+    end
+    
