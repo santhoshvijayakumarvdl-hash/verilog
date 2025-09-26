ece0e2f0244448253d8cd08f5f4f80dedf5fe601
santhoshvijayakumarvdl-hash <santhoshvijayakumarvdl@gmail.com>
2025-09-26 17:25:51 +0530

    Update tff.v

--- basics/day 2/tff.v
+++ basics/day 2/tff.v
@@ -10,3 +10,18 @@ output reg out
     1'b1:out<=out;
     endcase
     end
+      module tb;
+reg d,clk=0;
+wire out;
+              jjnn uut(d,clk,out);
+initial begin
+clk=0;
+end
+always@(*) begin
+forever #5 clk=~clk;
+end
+initial begin
+#5 t=0 ;
+#5 t=1 ;
+end 
+endmodule
