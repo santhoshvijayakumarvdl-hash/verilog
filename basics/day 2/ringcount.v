fd0c9099e28d0dcef2d90938ab2b36ba15907f68
santhoshvijayakumarvdl-hash <santhoshvijayakumarvdl@gmail.com>
2025-09-26 17:35:55 +0530

    Create ringcount.v

---
+++ basics/day 2/ringcount.v
@@ -0,0 +1,19 @@
+module jjnn ;
+    reg [7:0] count;
+    reg clk;
+    initial begin 
+    count=128;
+    clk=0;
+    end
+    always@(*)
+    forever #5 clk=~clk;
+    
+    
+    
+    always@(posedge clk) begin
+    count<=count>>1;
+    count[7]<=count[0];
+    end
+    
+    
+endmodule
