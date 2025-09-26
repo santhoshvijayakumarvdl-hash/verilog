2a3cdcea8736b544e37b237a880da34db4d64e3d
santhoshvijayakumarvdl-hash <santhoshvijayakumarvdl@gmail.com>
2025-09-26 17:15:26 +0530

    Update srff.v

--- basics/day 2/srff.v
+++ basics/day 2/srff.v
@@ -1,7 +1,26 @@
+module jjnn(input j,k,clk,
+output reg out
+    );
+    initial begin
+    out=0; end
+    
+    always@(posedge clk) begin
+      case({s,r})
+    2'b00:out<=out;
+    2'b01:out<=0;
+    2'b10:out<=1;
+    2'b11:out<=1'bx;
+    endcase
+    end
+
+  
+//testbench
+
+
 module tb;
 reg j,k,clk=0;
 wire out;
-jjnn uut(j,k,clk,out);
+  jjnn uut(s,r,clk,out);
 initial begin
 clk=0;
 end
@@ -9,9 +28,9 @@ always@(*) begin
 forever #5 clk=~clk;
 end
 initial begin
-#5 j=0 ; k=0;
-#5 j=0 ; k=1;
-#5 j=1 ; k=0;
-#5 j=1 ; k=1;
+#5 s=0 ; r=0;
+#5 s=0 ; r=1;
+#5 s=1 ; r=0;
+#5 s=1 ; r=1;
 end 
 endmodule
