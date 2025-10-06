d8a70c492f7081fd92665f05282b06c2a3160aec
santhoshvijayakumarvdl-hash <santhoshvijayakumarvdl@gmail.com>
2025-10-06 08:06:22 +0530

    Update ringer.v

--- basics/hdlbits/circuits/ringer.v
+++ basics/hdlbits/circuits/ringer.v
@@ -1,9 +1,8 @@
 module top_module (
     input ring,
     input vibrate_mode,
-    output ringer,   // Make sound
-    output motor     // Vibrate
-);
-    assign motor = ring & vibrate_mode;
+    output ringer,  
+    output motor     
+);assign motor = ring & vibrate_mode;
     assign ringer = ring & ~vibrate_mode;
 endmodule
