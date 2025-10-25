036ee64f71e8e0e06ae24eb873d98fb65107f6b6
santhoshvijayakumarvdl-hash <santhoshvijayakumarvdl@gmail.com>
2025-10-25 15:06:11 +0530

    Create 5.v

---
+++ basics/hdlbits/FSM/5.v
@@ -0,0 +1,31 @@
+module top_module(
+    input clk,
+    input reset,    // Asynchronous reset to state B
+    input in,
+    output out);//  
+
+    parameter A=0, B=1, C=2,D=3;
+    reg[3:0] state, next_state;
+
+    always @(*) begin 
+         case(state)// This is a sequential always block
+       A:next_state = in? B:A; 
+       B:next_state = in? B:C;
+       C:next_state = in? D:A;
+        D:next_state = in? B:C; 
+             
+        endcase// This is a combinational always block
+        
+        
+    end
+
+    always @(posedge clk) begin  
+        if(reset) state<=A;
+        else state<=next_state;
+
+    end
+
+    // Output logic
+    assign out = (state ==D);
+
+endmodule
