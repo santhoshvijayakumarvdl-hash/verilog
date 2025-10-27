0dc5c604d4268f359ccfabc0c0c9a74b47de984b
santhoshvijayakumarvdl-hash <santhoshvijayakumarvdl@gmail.com>
2025-10-27 11:38:56 +0530

    Create jk.v

---
+++ basics/hdlbits/circuits/sequential/jk.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output Q); 
+    always@(posedge clk)
+        case({j,k})
+        0:Q<=Q;
+        1:Q<=0;
+        2:Q<=1;
+        3:Q<=~Q;
+        endcase
+
+endmodule
